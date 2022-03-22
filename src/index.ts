2c9f80594c3fdeb3e6a5d8dd66ccfc15429173bb
zhangmingrui <mingrui.zhang@cyberserval.cn>
2022-03-22 11:44:53 +0800

    feat: 条件组合组件增加

--- src/index.ts
+++ src/index.ts
@@ -6,3 +6,9 @@ export { default as AgGrid } from './ag-grid';
 
 export type { ISelectDate } from './select-date';
 export { default as SelectDate, getTimeOption, getDefaultTimeOptions } from './select-date';
+
+export type { IRemoteSelect } from './remote-select';
+export { default as RemoteSelect } from './remote-select';
+
+export type { IConditions } from './conditions';
+export { default as Conditions } from './conditions';
