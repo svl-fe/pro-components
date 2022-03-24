7f99ac98c25c5bfc927a91388b1156402d120463
zhangmingrui <mingrui.zhang@cyberserval.cn>
2022-03-24 09:47:21 +0800

    feat: 下拉框数据远程获取

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,6 @@ export type { InfoCardProps } from './info-card';
 export { default as InfoCard } from './info-card';
 
 export { default as CheckboxTree } from './checkbox-tree';
+
+export type { IRemoteSelect } from './remote-select';
+export { default as RemoteSelect } from './remote-select';
